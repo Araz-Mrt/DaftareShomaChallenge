628b44757f6615e363f1ad4cfd67cf8c3948b696
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-08 01:41:49 +0330

    feat(infrastructure):add appdbcontext and order configurations

---
+++ DaftareShomaChallenge.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DaftareShomaChallenge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaftareShomaChallenge.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<OrderLine> OrderLines { get; set; }
+
+    #region overrides
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+    #endregion
+
+}
---
+++ DaftareShomaChallenge.Infrastructure/Persistence/Configurations/OrderConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using DaftareShomaChallenge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DaftareShomaChallenge.Infrastructure.Persistence.Configurations;
+
+public class OrderConfigurations : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasMany(o => o.OrderLines)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.Property(o => o.Number).IsRequired();
+        builder.Property(o => o.TotalPrice).IsRequired();
+        builder.Property(o => o.Date).IsRequired();
+    }
+}
