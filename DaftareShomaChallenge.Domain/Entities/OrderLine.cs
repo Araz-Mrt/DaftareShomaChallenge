f6d1871645b76784a8e8d347f7442a1b5dc8b351
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-08 21:30:27 +0330

    refactor(domain):add navigation properties and foreign keys

--- DaftareShomaChallenge.Domain/Entities/OrderLine.cs
+++ DaftareShomaChallenge.Domain/Entities/OrderLine.cs
@@ -3,6 +3,11 @@
 public class OrderLine : BaseEntity
 {
     public Product Product { get; set; }
+    public Order Order { get; set; }
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+
+
     public int Quantity { get; set; }
     public decimal Price { get; set; }
 }
--- DaftareShomaChallenge.Domain/Entities/Product.cs
+++ DaftareShomaChallenge.Domain/Entities/Product.cs
@@ -4,5 +4,4 @@ public class Product : BaseEntity
 {
     public decimal Price { get; set; }
     public string Title { get; set; }
-
 }
