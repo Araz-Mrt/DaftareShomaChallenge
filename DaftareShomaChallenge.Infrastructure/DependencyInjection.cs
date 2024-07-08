c5617e486b40c6d62cbb86456c6465dd70e0ad0a
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 00:18:38 +0330

    feat(infrastructure): register base services

---
+++ DaftareShomaChallenge.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using DaftareShomaChallenge.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DaftareShomaChallenge.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastrctureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+        });
+        return services;
+    }
+}
