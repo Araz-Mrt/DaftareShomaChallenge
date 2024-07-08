fe7727f295758261c3d4a847c7e6ebd4b2a8230a
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-08 23:47:48 +0330

    feat(shared):add shared layer and common result and messages

---
+++ DaftareShomaChallenge.Shared/Common/Messages.cs
@@ -0,0 +1,7 @@
+﻿namespace DaftareShomaChallenge.Shared.Common;
+
+public static class Messages
+{
+    public const string SuccessMessage = "عملیات با موفقیت انجام شد";
+    public const string ErrorMessage = "عملیات با شکست مواجه شد";
+}
---
+++ DaftareShomaChallenge.Shared/Common/Result.cs
@@ -0,0 +1,46 @@
+﻿using DaftareShomaChallenge.Shared.Common;
+
+public class Result
+{
+    internal Result(bool isSuccess, IEnumerable<string>? errors)
+    {
+        IsSuccess = isSuccess;
+        Description = isSuccess ? Messages.SuccessMessage : Messages.ErrorMessage;
+        Errors = errors != null ? errors.ToArray() : null;
+    }
+
+    public bool IsSuccess { get; init; }
+
+    public string[]? Errors { get; init; }
+    public string Description { get; init; }
+
+    public static Result Success()
+    {
+        return new Result(true, Array.Empty<string>());
+    }
+
+    public static Result Failure(IEnumerable<string>? errors = null)
+    {
+        return new Result(false, errors);
+    }
+
+    public static Result<TValue> Success<TValue>(TValue value) =>
+        new(value, true, null);
+
+
+    public static Result<TValue?> Failure<TValue>(TValue? value = default, string[]? error = null) =>
+        new(value, false, error);
+}
+
+
+public class Result<TData> : Result
+{
+
+    protected internal Result(TData? data, bool isSuccess, string[]? error)
+        : base(isSuccess, error)
+        => Data = data;
+
+    public TData? Data { get; }
+
+    public static implicit operator Result<TData>(TData data) => Success(data);
+}
