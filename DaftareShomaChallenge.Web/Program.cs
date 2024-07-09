b9773f39bf3dfdea3a8589a6eb766e0580b5019b
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 12:54:34 +0330

    feat(web): add productList minimal api

--- DaftareShomaChallenge.Web/Program.cs
+++ DaftareShomaChallenge.Web/Program.cs
@@ -1,15 +1,13 @@
 using DaftareShomaChallenge.Application;
+using DaftareShomaChallenge.Application.Contracts.Interfaces;
 using DaftareShomaChallenge.Infrastructure.Persistence.Initializers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
-
 
 
 // Configure the HTTP request pipeline.
@@ -20,35 +18,22 @@ if (app.Environment.IsDevelopment())
 }
 using (var scope = app.Services.CreateScope())
 {
-    var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
-    await initialiser.InitializeAsync();
-    await initialiser.SeedAsync(scope.ServiceProvider);
+    var initializer = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
+    await initializer.InitializeAsync();
+    await initializer.SeedAsync(scope.ServiceProvider);
 }
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/products", async (IProductService productService) =>
+    {
+        var result = await productService.GetProductsAsync();
+        return (result);
+    })
+    .WithName("GetProducts")
+    .WithOpenApi();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
