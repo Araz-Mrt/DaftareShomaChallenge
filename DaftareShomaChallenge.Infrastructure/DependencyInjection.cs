8cf070a3590d9dafb8b78c6e5d399f96bb21c392
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 18:25:09 +0330

    feat(infrastructure):implement and register productSaleReportService

--- DaftareShomaChallenge.Infrastructure/DependencyInjection.cs
+++ DaftareShomaChallenge.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,10 @@
-﻿using DaftareShomaChallenge.Domain.Repositories;
+﻿using DaftareShomaChallenge.Domain.Interfaces;
+using DaftareShomaChallenge.Domain.Repositories;
 using DaftareShomaChallenge.Infrastructure.Persistence;
 using DaftareShomaChallenge.Infrastructure.Persistence.Initializers;
 using DaftareShomaChallenge.Infrastructure.Persistence.Seeder;
 using DaftareShomaChallenge.Infrastructure.Repositories;
+using DaftareShomaChallenge.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,10 +16,10 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrasturctureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddScoped<IProductRepository,ProductRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<SeederService>();
         services.AddScoped<ApplicationDbContextInitializer>();
-        
+        services.AddScoped<IProductSaleReportService, ProductSaleReportService>();
         services.AddDbContext<ApplicationDbContext>(options =>
         {
             options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
---
+++ DaftareShomaChallenge.Infrastructure/Services/ProductSaleReportService.cs
@@ -0,0 +1,12 @@
+﻿using DaftareShomaChallenge.Domain.Interfaces;
+using DaftareShomaChallenge.Domain.Models;
+
+namespace DaftareShomaChallenge.Infrastructure.Services;
+
+public class ProductSaleReportService : IProductSaleReportService
+{
+    public Task<List<SoldProductCountModel>> GetProductSalesCountReportAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
