1bf99a9a38c89840eab76f65e6fc375eeb34266c
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 11:28:33 +0330

    fix(infrastructure):add missing navigationExpression for orderConfiguration

--- DaftareShomaChallenge.Infrastructure/Persistence/Configurations/OrderConfigurations.cs
+++ DaftareShomaChallenge.Infrastructure/Persistence/Configurations/OrderConfigurations.cs
@@ -9,7 +9,7 @@ public class OrderConfigurations : IEntityTypeConfiguration<Order>
     public void Configure(EntityTypeBuilder<Order> builder)
     {
         builder.HasMany(o => o.OrderLines)
-            .WithOne().HasForeignKey(o => o.OrderId)
+            .WithOne(o => o.Order).HasForeignKey(o => o.OrderId)
             .OnDelete(DeleteBehavior.Cascade);
         builder.Property(o => o.Number).HasMaxLength(20);
     }
