2a3cbc72a5b55f07ff724a32d7a5217bc4abb121
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 11:25:50 +0330

    feat(infrastructure):add dbinitializer and base seederservice

---
+++ DaftareShomaChallenge.Infrastructure/Persistence/Initializers/ApplicationDbContextInitializer.cs
@@ -0,0 +1,52 @@
+﻿using DaftareShomaChallenge.Infrastructure.Persistence.Seeder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DaftareShomaChallenge.Infrastructure.Persistence.Initializers;
+
+public class ApplicationDbContextInitializer
+{
+    private readonly ILogger<ApplicationDbContextInitializer> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public ApplicationDbContextInitializer(ILogger<ApplicationDbContextInitializer> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async Task InitializeAsync()
+    {
+        try
+        {
+            await _context.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+    public async Task SeedAsync(IServiceProvider serviceProvider)
+    {
+        try
+        {
+            await TrySeedAsync(serviceProvider);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+
+    public async Task TrySeedAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        // Then set current tenant so the right connectionstring is used
+        var _seederService = serviceProvider.GetRequiredService<SeederService>();
+        await _seederService.SeedAsync();
+    }
+}
---
+++ DaftareShomaChallenge.Infrastructure/Persistence/Seeder/SeederService.cs
@@ -0,0 +1,39 @@
+﻿using DaftareShomaChallenge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaftareShomaChallenge.Infrastructure.Persistence.Seeder;
+
+internal class SeederService
+{
+    private readonly ApplicationDbContext _context;
+
+    internal SeederService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    internal async Task SeedAsync()
+    {
+        await SeedProductsAsync();
+    }
+
+    private async Task SeedProductsAsync()
+    {
+        if (!await _context.Products.AnyAsync())
+        {
+            await _context.Products.AddRangeAsync(GenerateProducts());
+            await _context.SaveChangesAsync();
+        }
+    }
+    private List<Product> GenerateProducts()
+    {
+        return new List<Product>()
+        {
+            new ("p1",25000),
+            new ("p2",35000),
+            new ("p3",45000),
+            new ("p4",5000),
+            new ("p5",50000),
+        };
+    }
+}
