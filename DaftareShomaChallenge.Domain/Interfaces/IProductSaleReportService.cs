d35d415d0fc947b83065ea37295a766be03a72f3
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 18:23:53 +0330

    feat(domain):productSaleReportService interface

---
+++ DaftareShomaChallenge.Domain/Interfaces/IProductSaleReportService.cs
@@ -0,0 +1,8 @@
+﻿using DaftareShomaChallenge.Domain.Models;
+
+namespace DaftareShomaChallenge.Domain.Interfaces;
+
+public interface IProductSaleReportService
+{
+    Task<List<SoldProductCountModel>> GetProductSalesCountReportAsync();
+}
---
+++ DaftareShomaChallenge.Domain/Models/SoldProductCountModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DaftareShomaChallenge.Domain.Models;
+
+public class SoldProductCountModel
+{
+    public string ProductName { get; set; }
+    public int Count { get; set; }
+}
