8b23b2107bf5bc84f93e4e4b15c8f792e82678ba
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-10 00:42:36 +0330

    fix(infrastructure):change order. date to new name in using

--- DaftareShomaChallenge.Infrastructure/Services/ProductSaleReportService.cs
+++ DaftareShomaChallenge.Infrastructure/Services/ProductSaleReportService.cs
@@ -29,7 +29,7 @@ public class ProductSaleReportService : IProductSaleReportService
         var query = _context.OrderLines
             .Include(ol => ol.Product)
             .Include(ol => ol.Order)
-            .Where(ol => ol.Order.Date >= fromDate && ol.Order.Date <= toDate)
+            .Where(ol => ol.Order.OrderDate >= fromDate && ol.Order.OrderDate <= toDate)
             .AsQueryable();
 
         var result = await query
@@ -37,5 +37,10 @@ public class ProductSaleReportService : IProductSaleReportService
             .Select(g => new SoldProductCountModel(g.Key, g.Sum(ol => ol.Quantity)))
             .ToListAsync();
         return Result.Success(result);
+    }
+
+    public Task<Result<List<ProductSalesByDateReportModel>>> GetProductSalesForLastNDaysAsync(int days = 7)
+    {
+        throw new NotImplementedException();
     }
 }
