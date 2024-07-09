03b2ce693d5171fdac40b28b23845fa813be7e19
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-10 01:43:21 +0330

    feat(shared):add DateTimeExtension methods

---
+++ DaftareShomaChallenge.Shared/ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace DaftareShomaChallenge.Shared.ExtensionMethods;
+
+public static class DateTimeExtensions
+{
+    private static readonly PersianCalendar persianCalendar = new PersianCalendar();
+
+    public static string ToPersianDateString(this DateTime dateTime)
+    {
+        int year = persianCalendar.GetYear(dateTime);
+        int month = persianCalendar.GetMonth(dateTime);
+        int day = persianCalendar.GetDayOfMonth(dateTime);
+
+        return string.Concat(year.ToString("0000"), "/", month.ToString("00"), "/", day.ToString("00"));
+    }
+
+    public static string ToPersianMonthDayString(this DateTime dateTime)
+    {
+        int month = persianCalendar.GetMonth(dateTime);
+        int day = persianCalendar.GetDayOfMonth(dateTime);
+
+        return string.Concat(month.ToString("00"), "/", day.ToString("00"));
+    }
+}
