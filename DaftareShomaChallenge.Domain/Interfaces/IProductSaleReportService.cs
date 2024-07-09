9f08179b67d1174730a99e3a5f897dea63fb8087
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 21:59:36 +0330

    feat(domain): add GetProductSalesForLastNDays method and model

--- DaftareShomaChallenge.Domain/Interfaces/IProductSaleReportService.cs
+++ DaftareShomaChallenge.Domain/Interfaces/IProductSaleReportService.cs
@@ -4,5 +4,6 @@ namespace DaftareShomaChallenge.Domain.Interfaces;
 
 public interface IProductSaleReportService
 {
-    Task<Result<List<SoldProductCountModel>>> GetProductSalesCountReportAsync(DateTime fromDate,DateTime toDate);
+    Task<Result<List<SoldProductCountModel>>> GetProductSalesCountReportAsync(DateTime fromDate, DateTime toDate);
+    Task<Result<List<ProductSalesByDateReportModel>>> GetProductSalesForLastNDaysAsync(int days=7);
 }
---
+++ DaftareShomaChallenge.Domain/Models/ProductSalesByDateReportModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DaftareShomaChallenge.Domain.Models;
+
+public class ProductSalesByDateReportModel
+{
+    public string ProductName { get; set; }
+    public Dictionary<DateTime, int> SalesByDate { get; set; }
+}
