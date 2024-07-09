0bbdbf39d55bfe03d945049ef45b89bd591a154a
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-10 01:49:19 +0330

    refactor(infrastructure): wrap GetProductSalesCountReport method by try catch

--- DaftareShomaChallenge.Infrastructure/Services/ProductSaleReportService.cs
+++ DaftareShomaChallenge.Infrastructure/Services/ProductSaleReportService.cs
@@ -16,28 +16,35 @@ public class ProductSaleReportService : IProductSaleReportService
     }
     public async Task<Result<List<SoldProductCountModel>>> GetProductSalesCountReportAsync(DateTime fromDate, DateTime toDate)
     {
-
-        if (fromDate.Date == toDate.Date)
+        try
         {
-            //main way:
-            // toDate = toDate.Date.AddDays(1).AddTicks(-1);
+            if (fromDate.Date == toDate.Date)
+            {
+                //main way:
+                // toDate = toDate.Date.AddDays(1).AddTicks(-1);
 
-            //because of the sqliteProvider
-            toDate = toDate.Date.AddDays(1).Date;
+                //because of the sqliteProvider
+                toDate = toDate.Date.AddDays(1).Date;
+            }
+
+
+            var query = _context.OrderLines
+                .Include(ol => ol.Product)
+                .Include(ol => ol.Order)
+                .Where(ol => ol.Order.OrderDate >= fromDate && ol.Order.OrderDate <= toDate)
+                .AsQueryable();
+
+            var result = await query
+                .GroupBy(ol => ol.Product.Title)
+                .Select(g => new SoldProductCountModel(g.Key, g.Sum(ol => ol.Quantity)))
+                .ToListAsync();
+            return Result.Success(result);
+        }
+        catch (Exception e)
+        {
+            return Result.Failure<List<SoldProductCountModel>>(null, [e.Message]);
         }
 
-
-        var query = _context.OrderLines
-            .Include(ol => ol.Product)
-            .Include(ol => ol.Order)
-            .Where(ol => ol.Order.OrderDate >= fromDate && ol.Order.OrderDate <= toDate)
-            .AsQueryable();
-
-        var result = await query
-            .GroupBy(ol => ol.Product.Title)
-            .Select(g => new SoldProductCountModel(g.Key, g.Sum(ol => ol.Quantity)))
-            .ToListAsync();
-        return Result.Success(result);
     }
 
     public async Task<Result<List<ProductSalesByDateReportModel>>> GetProductSalesForLastNDaysAsync(int days = 7)
@@ -50,7 +57,7 @@ public class ProductSaleReportService : IProductSaleReportService
                  GenerateSqlQuery(),
                  new SqliteParameter("@TargetDay", dateOffset))
              .ToListAsync();
-            return Result.Success<List<ProductSalesByDateReportModel>>(result);
+            return Result.Success(result);
         }
         catch (Exception e)
         {
