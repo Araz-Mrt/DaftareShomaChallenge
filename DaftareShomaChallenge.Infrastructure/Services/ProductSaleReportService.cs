d9c95e6179f962085e1451d1f97d23b740c6bcd1
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 19:50:55 +0330

    feat(infrastructure):implement GetProductSalesCountReportAsync method

--- DaftareShomaChallenge.Infrastructure/Services/ProductSaleReportService.cs
+++ DaftareShomaChallenge.Infrastructure/Services/ProductSaleReportService.cs
@@ -1,12 +1,35 @@
 ﻿using DaftareShomaChallenge.Domain.Interfaces;
 using DaftareShomaChallenge.Domain.Models;
+using DaftareShomaChallenge.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace DaftareShomaChallenge.Infrastructure.Services;
 
 public class ProductSaleReportService : IProductSaleReportService
 {
-    public Task<Result<List<SoldProductCountModel>>> GetProductSalesCountReportAsync(DateTime fromDate, DateTime toDate)
+    private readonly ApplicationDbContext _context;
+
+    public ProductSaleReportService(ApplicationDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    public async Task<Result<List<SoldProductCountModel>>> GetProductSalesCountReportAsync(DateTime fromDate, DateTime toDate)
+    {
+        if (fromDate.Date == toDate.Date)
+        {
+            toDate = toDate.Date.AddDays(1).AddTicks(-1);
+        }
+
+        var query = _context.OrderLines
+            .Include(ol => ol.Product)
+            .Include(ol => ol.Order)
+            .Where(ol => ol.Order.Date >= fromDate && ol.Order.Date <= toDate)
+            .AsQueryable();
+
+        var result = await query
+            .GroupBy(ol => ol.Product.Title)
+            .Select(g => new SoldProductCountModel(g.Key, g.Sum(ol => ol.Quantity)))
+            .ToListAsync();
+        return Result.Success(result);
     }
 }
