5f6c26a7c36bc5a44685fd7e3a06e56f28276e24
Araz-Mrt <morovvati.dev@gmail.com>
2024-07-09 17:43:00 +0330

    feat(infrastructure):add migration for order date

--- DaftareShomaChallenge.Infrastructure/Migrations/20240709141222_Add_Date_To_Order.Designer.cs
+++ DaftareShomaChallenge.Infrastructure/Migrations/20240709141222_Add_Date_To_Order.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DaftareShomaChallenge.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DaftareShomaChallenge.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240709141222_Add_Date_To_Order")]
+    partial class Add_Date_To_Order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
